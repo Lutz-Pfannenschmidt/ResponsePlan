515ee8c179b1fc2169613e245be690cb7263d0d9
Lutz Pfannenschmidt <lutz@lutz-pfannenschmidt.de>
2023-12-16 12:08:33 +0100

    Add additional logging methods to Logger class

--- internal/logging/main.go
+++ internal/logging/main.go
@@ -2,6 +2,7 @@ package logging
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/fatih/color"
 )
@@ -33,4 +34,69 @@ func (l *Logger) Debug(message string) {
 // Error logs a message to the console as an error.
 func (l *Logger) Error(message string) {
 	fmt.Println(color.RedString(message))
+}
+
+// Fatal logs a message to the console as an error and exits the program.
+func (l *Logger) Fatal(message string) {
+	fmt.Println(color.RedString(message))
+	os.Exit(1)
+}
+
+// FatalErr logs an error to the console as an error and exits the program.
+func (l *Logger) FatalErr(err error) {
+	fmt.Println(color.RedString(err.Error()))
+	os.Exit(1)
+}
+
+// Success logs a message to the console as a success.
+func (l *Logger) Success(message string) {
+	fmt.Println(color.GreenString(message))
+}
+
+// Warning logs a message to the console as a warning.
+func (l *Logger) Warning(message string) {
+	fmt.Println(color.YellowString(message))
+}
+
+// Info logs a message to the console as an info.
+func (l *Logger) Info(message string) {
+	fmt.Println(color.BlueString(message))
+}
+
+// Logf logs a formatted message to the console.
+func (l *Logger) Logf(format string, a ...interface{}) {
+	fmt.Printf(color.WhiteString(format), a...)
+}
+
+// Debugf logs a formatted message to the console if debug is enabled.
+func (l *Logger) Debugf(format string, a ...interface{}) {
+	if l.debug {
+		fmt.Printf(color.MagentaString(format), a...)
+	}
+}
+
+// Errorf logs a formatted message to the console as an error.
+func (l *Logger) Errorf(format string, a ...interface{}) {
+	fmt.Printf(color.RedString(format), a...)
+}
+
+// Fatalf logs a formatted message to the console as an error and exits the program.
+func (l *Logger) Fatalf(format string, a ...interface{}) {
+	fmt.Printf(color.RedString(format), a...)
+	os.Exit(1)
+}
+
+// Successf logs a formatted message to the console as a success.
+func (l *Logger) Successf(format string, a ...interface{}) {
+	fmt.Printf(color.GreenString(format), a...)
+}
+
+// Warningf logs a formatted message to the console as a warning.
+func (l *Logger) Warningf(format string, a ...interface{}) {
+	fmt.Printf(color.YellowString(format), a...)
+}
+
+// Infof logs a formatted message to the console as an info.
+func (l *Logger) Infof(format string, a ...interface{}) {
+	fmt.Printf(color.BlueString(format), a...)
 }
