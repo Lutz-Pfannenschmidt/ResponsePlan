bc46f7081326467cef7c5591103a75bce7903835
Lutz Pfannenschmidt <lutz@lutz-pfannenschmidt.de>
2023-12-18 17:46:56 +0100

    Update NetworkVisualizer to display multiple addresses for each host, added README

--- web/src/components/NetworkVisualizer.ts
+++ web/src/components/NetworkVisualizer.ts
@@ -167,7 +167,7 @@ function updateSvg() {
     text.setAttribute("y", `${20 + height + 40 * i}`);
     text.classList.add("fill-base-content")
     text.dataset.index = `${i}`;
-    text.textContent = host.os.os_matches != null ? host.os.os_matches[0].name + " (" + host.os.os_matches[0].accuracy + "% accuracy)" : host.addresses.find(address => address.addr_type == "ipv4")?.addr || "Unknown";
+    text.textContent = host.addresses.map(address => address.addr).join(", ");
     svg.appendChild(text);
   });
 
