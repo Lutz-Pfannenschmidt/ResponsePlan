0d9d49ed3e5ad66925305d3122a547e95b16676a
Lutz Pfannenschmidt <lutz@lutz-pfannenschmidt.de>
2024-02-18 21:51:50 +0100

    refactor template rendering

---
+++ internal/htmx/render.go
@@ -0,0 +1,237 @@
+package htmx
+
+import (
+	"bytes"
+	"embed"
+	"encoding/json"
+	"html/template"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/Lutz-Pfannenschmidt/ResponsePlan/internal/scans"
+	"github.com/Lutz-Pfannenschmidt/ResponsePlan/internal/svg"
+	"github.com/Lutz-Pfannenschmidt/yagll"
+	"github.com/google/uuid"
+	"github.com/julienschmidt/httprouter"
+)
+
+type Renderer struct {
+	devMode *bool
+	fs      *embed.FS
+	sm      *scans.ScanManager
+}
+
+func NewRenderer(devMode *bool, fs *embed.FS, sm *scans.ScanManager) *Renderer {
+	return &Renderer{
+		devMode: devMode,
+		fs:      fs,
+		sm:      sm,
+	}
+}
+
+// ServeComponent serves a component with the given title and data.
+// data is optional and can be used to pass data to the component.
+// The component is expected to be in the "components" directory.
+func (r *Renderer) ServeComponent(title, component string, data ...map[string]any) func(w http.ResponseWriter, rq *http.Request, ps httprouter.Params) {
+	return func(w http.ResponseWriter, rq *http.Request, ps httprouter.Params) {
+		tpl, err := r.recursiveParseComponent("index.html").ParseFS(r.fs, "templates/index.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			yagll.Errorf("Error parsing component template: %s", err)
+			return
+		}
+
+		buf := &bytes.Buffer{}
+
+		data := map[string]interface{}{
+			"Title":     title,
+			"Component": "components/" + component,
+		}
+
+		err = tpl.Execute(buf, data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			yagll.Errorf("Error executing component template: %s", err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/html")
+		w.Write(buf.Bytes())
+	}
+}
+
+// ServeComponentX serves a component with the given data.
+// data is optional and can be used to pass data to the component.
+// The component is expected to be in the "components" directory.
+func (r *Renderer) ServeComponentX(component string, data ...map[string]any) func(w http.ResponseWriter, rq *http.Request, ps httprouter.Params) {
+	return func(w http.ResponseWriter, rq *http.Request, ps httprouter.Params) {
+		tpl, err := r.recursiveParseComponent("index.html").ParseFS(r.fs, "templates/components/"+component)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			yagll.Errorf("Error parsing component template: %s", err)
+			return
+		}
+
+		buf := &bytes.Buffer{}
+
+		dat := map[string]interface{}{}
+		if len(data) > 0 {
+			dat = data[0]
+		}
+
+		err = tpl.Execute(buf, dat)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			yagll.Errorf("Error executing component template: %s", err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/html")
+		w.Write(buf.Bytes())
+	}
+}
+
+func (r *Renderer) ServeJSON(data interface{}) func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		str, err := json.Marshal(data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			yagll.Errorf("Error marshalling JSON: %s", err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(str)
+	}
+}
+
+func (r *Renderer) ServeErrorPage(errNum int, msg string) func(w http.ResponseWriter, rq *http.Request) {
+	return func(w http.ResponseWriter, rq *http.Request) {
+		tpl, err := r.recursiveParseComponent("index.html").ParseFS(r.fs, "templates/index.html")
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			yagll.Errorf("Error parsing component template: %s", err)
+			return
+		}
+
+		buf := &bytes.Buffer{}
+
+		data := map[string]interface{}{
+			"Error":        err,
+			"ErrorMessage": msg,
+			"Title":        "Error " + strconv.Itoa(errNum),
+		}
+
+		err = tpl.Execute(buf, data)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			yagll.Errorf("Error executing component template: %s", err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/html")
+		w.Write(buf.Bytes())
+	}
+}
+
+func (r *Renderer) RenderComponent(component string, icon bool, data ...string) template.HTML {
+	var dat interface{}
+
+	component = strings.ReplaceAll(component, "components/", "")
+	component = strings.ReplaceAll(component, "icons/", "")
+
+	folder := "components"
+	if icon {
+		folder = "icons"
+	}
+
+	if len(data) > 0 {
+		err := json.Unmarshal([]byte(data[0]), &dat)
+		if err != nil {
+			yagll.Errorf("Error unmarshalling component data: %s", err)
+			return template.HTML("Error unmarshalling component data")
+		}
+	}
+
+	tpl, err := r.recursiveParseComponent(component).ParseFS(r.fs, "templates/"+folder+"/"+component)
+	if err != nil {
+		yagll.Errorf("Error parsing component template: %s", err)
+		return template.HTML("Error parsing component template")
+	}
+
+	buf := &bytes.Buffer{}
+
+	err = tpl.Execute(buf, dat)
+	if err != nil {
+		yagll.Errorf("Error executing component template: %s", err)
+		return template.HTML("Error executing component template")
+	}
+
+	return template.HTML(buf.String())
+}
+
+// RenderGraph renders a scan based on the given ID.
+// If no ID is given, the latest scan is rendered.
+// If multiple IDs are given, the first one is used.
+func (r *Renderer) RenderGraph(id ...string) template.HTML {
+	if len(id) > 0 {
+		uid, err := uuid.Parse(id[0])
+		if err != nil {
+			yagll.Errorf("Error parsing UUID: %s", err)
+			return template.HTML("Error parsing UUID")
+		}
+
+		return template.HTML(svg.OverwriteRunToSvg(r.sm, uid))
+	} else {
+		if len(r.sm.Scans) == 0 {
+			return template.HTML("No scans")
+		}
+
+		var latestScanId uuid.UUID
+		var latestScanTime int64
+		for id, scan := range r.sm.Scans {
+			if scan != nil && scan.EndTime > latestScanTime {
+				latestScanTime = scan.EndTime
+				latestScanId = id
+			}
+		}
+
+		return template.HTML(svg.OverwriteRunToSvg(r.sm, latestScanId))
+	}
+
+}
+
+func (r *Renderer) recursiveParseComponent(component string) *template.Template {
+
+	split := strings.Split(component, "/")
+	fname := "index.html"
+	if len(split) >= 1 {
+		fname = split[len(split)-1]
+	}
+
+	tpl := template.New(fname)
+	tpl.Funcs(template.FuncMap{
+		"component": func(component string, data ...string) template.HTML {
+			return r.RenderComponent(component, false, data...)
+		},
+		"icon": func(component string, data ...string) template.HTML {
+			return r.RenderComponent(component, true, data...)
+		},
+		"string":   mustMarshal,
+		"svg":      r.RenderGraph,
+		"allScans": func() map[uuid.UUID]*scans.Scan { return r.sm.Scans },
+	})
+
+	return tpl
+}
+
+func mustMarshal(data interface{}) string {
+	str, err := json.Marshal(data)
+	if err != nil {
+		yagll.Errorf("Error marshalling data: %s", err)
+		return "Error marshalling data"
+	}
+
+	return string(str)
+}
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"embed"
 	"encoding/json"
 	"fmt"
@@ -11,11 +10,9 @@ import (
 	"os"
 	"os/signal"
 	"strconv"
-	"strings"
 
 	"github.com/Lutz-Pfannenschmidt/ResponsePlan/internal/htmx"
 	"github.com/Lutz-Pfannenschmidt/ResponsePlan/internal/scans"
-	"github.com/Lutz-Pfannenschmidt/ResponsePlan/internal/svg"
 	"github.com/Lutz-Pfannenschmidt/yagll"
 	"github.com/google/uuid"
 
@@ -32,158 +29,7 @@ var cdn, _ = fs.Sub(cdnFs, "cdn")
 
 var devMode = false
 var scanManager = scans.NewScanManager()
-
-func attachTemplateFunctions(t *template.Template) *template.Template {
-	return t.Funcs(template.FuncMap{
-		"component": func(name string, data ...string) template.HTML {
-
-			input := ""
-			if len(data) >= 1 {
-				input = strings.TrimSpace(data[0])
-			}
-
-			dat := map[string]interface{}{}
-
-			if input != "" {
-				err := json.Unmarshal([]byte(input), &dat)
-				if err != nil {
-					yagll.Errorf("Error parsing data: %s", err.Error())
-					yagll.Errorf("Data: %s", input)
-					return template.HTML("Error parsing data")
-				}
-			}
-
-			nameSplit := strings.Split(name, "/")
-			t, err := attachTemplateFunctions(template.New(nameSplit[len(nameSplit)-1])).ParseFS(templates, "templates/"+name)
-			if err != nil {
-				yagll.Errorf("Error parsing template: %s", err.Error())
-				return template.HTML("Error parsing template")
-			}
-
-			var tpl bytes.Buffer
-
-			if err := t.Execute(&tpl, dat); err != nil {
-				yagll.Errorf("Error executing template: %s", err.Error())
-				return template.HTML("Error executing template")
-			}
-
-			response := template.HTML(tpl.String())
-			return response
-		},
-		"string": func(data any) string {
-			text, err := json.Marshal(data)
-			if err != nil {
-				yagll.Errorf("Error parsing data")
-				return "Error passing data"
-			}
-			return string(text)
-		},
-		"svg": func() template.HTML {
-			if len(scanManager.Scans) == 0 {
-				return template.HTML("No scans")
-			}
-
-			var latestScanId uuid.UUID
-			var latestScanTime int64
-			for id, scan := range scanManager.Scans {
-				if scan != nil && scan.EndTime > latestScanTime {
-					latestScanTime = scan.StartTime
-					latestScanId = id
-				}
-			}
-
-			if latestScanId == uuid.Nil {
-				return template.HTML("No scans")
-			}
-
-			return template.HTML(svg.OverwriteRunToSvg(scanManager, latestScanId))
-		},
-		"allScans": func() map[uuid.UUID]*scans.Scan {
-			return scanManager.Scans
-		},
-	})
-}
-
-func getErrorHandler(error int, message string) func(w http.ResponseWriter, r *http.Request) {
-	return func(w http.ResponseWriter, r *http.Request) {
-		tpl, err := attachTemplateFunctions(template.New("index.html")).ParseFS(templates, "templates/index.html")
-
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("500 - Something bad happened!"))
-			return
-		}
-
-		errorData := map[string]interface{}{
-			"Error":        error,
-			"ErrorMessage": message,
-			"Title":        "Error " + strconv.Itoa(error),
-		}
-
-		tpl.Execute(w, errorData)
-	}
-}
-
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-
-	tpl, err := attachTemplateFunctions(template.New("index.html")).ParseFS(templates, "templates/index.html")
-
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("500 - Something bad happened!"))
-		return
-	}
-
-	testData := map[string]interface{}{
-		"Name":  "Lutz Pfannenschmidt",
-		"Title": "Home",
-		"Test":  []int{1, 2, 3, 4, 5},
-		"Dev":   devMode,
-		"Next":  true,
-	}
-
-	tpl.Execute(w, testData)
-}
-
-func IndexWithComponent(title string, component string) func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-
-		tpl, err := attachTemplateFunctions(template.New("index.html")).ParseFS(templates, "templates/index.html")
-
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("500 - Something bad happened!"))
-			return
-		}
-
-		testData := map[string]interface{}{
-			"Title":     title,
-			"Component": component,
-		}
-
-		tpl.Execute(w, testData)
-	}
-}
-
-func Component(title string, component string) func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	return func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-
-		split := strings.Split(component, "/")
-		fname := split[len(split)-1]
-		tpl, err := attachTemplateFunctions(template.New(fname)).ParseFS(templates, "templates/"+component)
-
-		if err != nil {
-			panic(err)
-		}
-
-		testData := map[string]interface{}{
-			"Title":     title,
-			"Component": component,
-		}
-
-		tpl.Execute(w, testData)
-	}
-}
+var renderer *htmx.Renderer
 
 func StartScan(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	yagll.Debugln("Starting scan")
@@ -265,19 +111,13 @@ func MakeDeviceInfoHandler(jsonOnly bool) func(w http.ResponseWriter, r *http.Re
 			return
 		}
 
-		tpl, err := attachTemplateFunctions(template.New("deviceInfo.html")).ParseFS(templates, "templates/components/deviceInfo.html")
-		if err != nil {
-			yagll.Errorf("Error parsing template: %s", err.Error())
-			return
-		}
-
 		jsonHost, err := json.MarshalIndent(host, "", "  ")
 		if err != nil {
 			yagll.Errorf("Error parsing json: %s", err.Error())
 			return
 		}
 
-		testData := map[string]interface{}{
+		data := map[string]interface{}{
 			"Title": "Device Info",
 			"Host":  host,
 			"Json":  template.HTML(string(jsonHost)),
@@ -285,7 +125,15 @@ func MakeDeviceInfoHandler(jsonOnly bool) func(w http.ResponseWriter, r *http.Re
 			"IDX":   idx,
 		}
 
-		tpl.Execute(w, testData)
+		stringData, err := json.Marshal(data)
+		if err != nil {
+			yagll.Errorf("Error marshalling data: %s", err.Error())
+			return
+		}
+
+		html := renderer.RenderComponent("DeviceInfo", false, string(stringData))
+		w.Header().Set("Content-Type", "text/html")
+		w.Write([]byte(html))
 	}
 }
 
@@ -327,21 +175,22 @@ func main() {
 	}
 
 	router := httprouter.New()
+	renderer = htmx.NewRenderer(&devMode, &templates, scanManager)
 
 	// Serve the CDN
 	router.ServeFiles("/cdn/*filepath", http.FS(cdn))
 
 	// Routes
-	router.GET("/", IndexWithComponent("Graph", "components/graph.html"))
-	router.GET("/graph", IndexWithComponent("Graph", "components/graph.html"))
-	router.GET("/analytics", IndexWithComponent("Analytics", "components/analytics.html"))
-	router.GET("/history", IndexWithComponent("History", "components/history.html"))
+	router.GET("/", renderer.ServeComponent("Graph", "graph.html"))
+	router.GET("/graph", renderer.ServeComponent("Graph", "graph.html"))
+	router.GET("/analytics", renderer.ServeComponent("Analytics", "analytics.html"))
+	router.GET("/history", renderer.ServeComponent("History", "history.html"))
 
 	// HTMX component routes
-	router.GET("/x/", Component("Graph", "components/graph.html"))
-	router.GET("/x/graph", Component("Graph", "components/graph.html"))
-	router.GET("/x/analytics", Component("Analytics", "components/analytics.html"))
-	router.GET("/x/history", Component("History", "components/history.html"))
+	router.GET("/x/", renderer.ServeComponentX("graph.html"))
+	router.GET("/x/graph", renderer.ServeComponentX("graph.html"))
+	router.GET("/x/analytics", renderer.ServeComponentX("analytics.html"))
+	router.GET("/x/history", renderer.ServeComponentX("history.html"))
 	router.GET("/x/deviceInfo/:uuid/:idx", MakeDeviceInfoHandler(false))
 	router.GET("/x/runningScans", RunningScans)
 
@@ -350,12 +199,12 @@ func main() {
 	router.GET("/api/deviceJson/:uuid/:idx", MakeDeviceInfoHandler(true))
 
 	// Error handling
-	router.NotFound = http.HandlerFunc(getErrorHandler(404, "Page not found"))
+	router.NotFound = http.HandlerFunc(renderer.ServeErrorPage(404, "Page not found"))
 	router.PanicHandler = func(w http.ResponseWriter, r *http.Request, err interface{}) {
 		yagll.Errorf("Panic: %s", err)
-		getErrorHandler(500, "Internal server error")(w, r)
+		renderer.ServeErrorPage(500, "Internal server error")(w, r)
 	}
-	router.MethodNotAllowed = http.HandlerFunc(getErrorHandler(405, "Method not allowed"))
+	router.MethodNotAllowed = http.HandlerFunc(renderer.ServeErrorPage(405, "Method not allowed"))
 
 	yagll.Debugln("Setting up SIGINT handler")
 	c := make(chan os.Signal, 1)
